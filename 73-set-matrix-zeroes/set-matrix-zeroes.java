af0530db211afdf9701838c0ffee86232414d941
PRANESH RAJA V K <138082712+praneshraja23@users.noreply.github.com>
2024-05-17 21:41:01 -0400

    Update set-matrix-zeroes.java

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -26,8 +26,6 @@ class Solution {
     public void setZeroRow(int[][] matrix,int row,int cols,int col,boolean[][] arr){
         int rows=matrix.length;
         for(int i=0;i<cols;i++){
-            // if(matrix[row][i]==0 && i!=col && arr[row][i]==true)
-            //  setZeroCol(matrix,rows,i,row,arr);
             if(matrix[row][i]!=0)
              matrix[row][i]=0;
         }
@@ -35,8 +33,6 @@ class Solution {
      public void setZeroCol(int[][] matrix,int rows,int col,int row,boolean[][] arr){
         int cols=matrix[0].length;
         for(int i=0;i<rows;i++){
-            // if(matrix[i][col]==0 && i!=row && arr[row][i]==true)
-            //   setZeroRow(matrix,i,cols,col,arr);
             if(matrix[i][col]!=0)
               matrix[i][col]=0;
         }
