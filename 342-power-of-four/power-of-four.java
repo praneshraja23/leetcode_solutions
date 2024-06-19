1677711b1b23c82a0929f1bd65f0ec6d1ccc8d66
PRANESH RAJA V K <138082712+praneshraja23@users.noreply.github.com>
2024-06-18 23:04:48 -0400

    Time: 1 ms (27.43%) | Memory: 40.3 MB (94.40%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -2,10 +2,10 @@ class Solution {
     public boolean isPowerOfFour(int n) {
        if(n==1)
        return true;
-       double s=n;
-       while(s!=1.0 && s>=4.0){
-        s/=4;
-       } 
-       return s==1.0;
+    //    double s=n;
+    //    while(s!=1.0 && s>=4.0){
+    //     s/=4;
+    //    } 
+       return (Math.log(n)/Math.log(4))%1==0;
     }
 }
