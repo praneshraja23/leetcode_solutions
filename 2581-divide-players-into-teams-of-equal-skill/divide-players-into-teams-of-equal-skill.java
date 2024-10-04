351a04eb52203da46417e5e42e1f988c375bfcfc
PRANESH RAJA V K <138082712+praneshraja23@users.noreply.github.com>
2024-10-04 12:42:59 -0400

    Time: 1563 ms (5.54%) | Memory: 57.8 MB (39.29%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -6,18 +6,18 @@ class Solution {
             return skill[0]*skill[1];
         }
         int sum=0;
-        List<Integer> list=new ArrayList<>();
         for(int num:skill)
         {
             sum+=num;
-            list.add(num);
         }
 
         int target=sum/(len/2);
+
         if(sum%(len/2)!=0)
         {
             return -1;
         }
+
         long che=0,count=1;
         for(int i=0;i<len;i++)
         {   
@@ -27,7 +27,7 @@ class Solution {
             int s=0;
             for(int j=i+1;j<len;j++)
             {   
-                if(skill[j]==val && skill[j]!=-1)
+                if(skill[j]==val)
                 {
                    s=1;
                    che+=(skill[i]*val);
