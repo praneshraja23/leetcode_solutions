21adce1b7e290b16000592b172b178823307393e
PRANESH RAJA V K <138082712+praneshraja23@users.noreply.github.com>
2024-11-03 01:54:22 -0400

    Time: 1 ms (71.04%) | Memory: 41.9 MB (88.20%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+         List<List<Integer>> lists=new ArrayList<>();
+        if(root==null)
+        {
+            return lists;
+        }
+        Deque<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        boolean rev=false;
+        while(!queue.isEmpty())
+        {
+            int size=queue.size();
+            List<Integer> list=new ArrayList<>();
+            if(rev)
+            {
+                 for(int i=0;i<size;i++)
+                {
+                    TreeNode currentNode=queue.pollLast();
+                    list.add(currentNode.val);
+                     
+                    if(currentNode.right!=null)
+                    {
+                        queue.addFirst(currentNode.right);
+                    }
+                    if(currentNode.left!=null)
+                    {
+                        queue.addFirst(currentNode.left);
+                    }
+                   
+                    
+                }
+            }
+            else
+            {
+                for(int i=0;i<size;i++)
+                {
+                    TreeNode currentNode=queue.pollFirst();
+                    list.add(currentNode.val);
+                    if(currentNode.left!=null)
+                    {
+                        queue.addLast(currentNode.left);
+                    }
+                    if(currentNode.right!=null)
+                    {
+                        queue.addLast(currentNode.right);
+                    }
+                    
+                }
+            }
+            rev=!rev;
+            lists.add(list);
+        }
+        return lists;
+    }
+}
