58faa5de0ae67d54fd6b6098d07dc73a5c852b81
PRANESH RAJA V K <138082712+praneshraja23@users.noreply.github.com>
2024-11-30 21:07:05 -0500

    Time: 2 ms (93.52%) | Memory: 43.1 MB (33.23%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,36 +1,13 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
-        int len=arr.length;
-        if(len==2)
-        {if(arr[0]==2*arr[1])
-          return true;
-         else
-          return false;}
-        boolean a=false;
-        for(int i=len-1;i>=0;i--){
-           if(i-1!=0) 
-           a=binarysearch(arr,0,i-1,arr[i]);
-           if(a)
-             return a;
-           if(i+1!=len-1)
-           a=binarysearch(arr,i+1,len-1,arr[i]);
-           if(a)
-             return a;
-        }
-        return false;
-    }
-    public boolean binarysearch(int[] arr,int start,int end,int val){
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(2*arr[mid]==val){
-                return true;
-            }
-            else if(2*arr[mid]>val){
-                end=mid-1;
-            }
-            else{
-                start=mid+1;
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length;j++)
+            {   if(i==j) continue;
+                if(arr[i]==2*arr[j])
+                {
+                    return true;
+                }
             }
         }
         return false;
